8eee7d667fff08efd7b081bc1c765f49bfa29f0a
gandis <gandis0713@gmail.com>
2018-08-19 14:06:34 +0900

    modify mbr.asm

--- assembly/mbr/mbr.asm
+++ assembly/mbr/mbr.asm
@@ -8,23 +8,29 @@ Initialize:
 	mov ax, cs
 	mov ds, ax	; set data segment address to code segment address.
 
+	mov ax, 0xB800
+	mov es, ax	; set display address.
+	
+	mov ax, 0x0000
+	mov ss, ax	; initialize Stack Segment address.
+	mov bp, 0xFFFF	; initialize Stack Base Pointer address.
+	mov sp, 0xFFFF	; initialize Stack Pointer address.
+
 	call CleanDisplay
-	call SetRegistersForPrint
-	call Print
+	call StartBooting
 
 CleanDisplay:
 	call SetRegistersForCleanDisplay
 	call CleanLoop
 
+	ret
+
 SetRegistersForCleanDisplay:
-	mov ax, 0xB800
-	mov es, ax	; set display address.
-
 	mov cx, 0x07D0	; set count by display size. ( 0x0080 * 0x0025 * 0x0002 )
-
 	mov di, 0x0000	; initialize the offset address for target text.
-
 	mov ax, word [msgClean]	; set text property as 2 byte.
+
+	ret
 
 CleanLoop:
 	mov word [ es : di ], ax
@@ -33,56 +39,59 @@ CleanLoop:
 
 	jnz CleanLoop
 
-SetRegistersForPrint:
-	mov ax, 0x0000
-	mov ss, ax
-	mov bp, 0xFFFF
-	mov sp, 0xFFFF
+	ret
 
-	mov si, 0x0000
-	mov di, 0x0000
+StartBooting:
+
+	push msgStart
+	push 0x0000
+	push 0x0000
+
+	call Print
+	call Done
 
 Print:
-	push msgStart
-	push 0
-	push 0
+	mov si, 0x0000
+	mov di, 0x0000
 
 	push bp
 	mov bp, sp
 
-	mov di, 0
+	mov di, 0x0000
 
-	mov ax, word [ bp + 2 ]
-	mov si, 2
+	mov ax, word [ bp + 0x0004 ]
+	mov si, 0x0002
 	mul si
 	add di, ax
 
-	mov ax, word [ bp + 4 ]
-	mov si, 160
+	mov ax, word [ bp + 0x0006 ]
+	mov si, 0x0160
 	mul si
 	add di, ax
 
-	mov si, word [ bp + 6 ]
-MessageLoop:
+	mov si, word [ bp + 0x0008 ]
+.PrintLoop:
 
 	mov cl, byte [ si ]
 
-	cmp cl, 0
+	cmp cl, 0x000
 
 	je Done
 
 	mov byte [ es : di ], cl
 	inc si
-	add di, 2
+	add di, 0x0002
 
-	jmp MessageLoop
+	jmp .PrintLoop
+
+	pop bp
+	ret
 Done:
 	jmp $
-
+	
 msgClean dw 0x0A00
-msgStart db "gandis's os is started.", 0
+msgStart db "gandis's os is started.", 0x0000
 
-times 510 - ( $ - $$ ) db 0x0
+times 0x01FE - ( $ - $$ ) db 0x0000
 
-db 0x55
-db 0xAA
+dw 0xAA55
