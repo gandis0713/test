6d579cd2cb14be3f953a9bda26f074e6cdd5bf96
gandis <gandis0713@gmail.com>
2018-12-01 19:08:16 +0900

    create project for logic circuit

---
+++ logic_circuit/DNumberSystems.h
@@ -0,0 +1,4 @@
+#define BN(x) 0b##x            // binary
+#define OC(x) 0##x             // octal
+#define DE(x) x                // decimal
+#define HEXDE(x) 0x##x         // hexidecimal
---
+++ logic_circuit/main.cpp
@@ -0,0 +1,11 @@
+#include "DLogicGate.h"
+#include "DNumberSystems.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+
+    return 0;
+}
