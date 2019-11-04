c287e9d4fcfe975b8bba2d37a87d87d91823537a
gandis <gandis0713@gmail.com>
2019-11-04 22:19:10 +0900

    implemented component for graph

--- algorithm/hackerrank/disjoint_set/conponentsinagraph/main.cpp
+++ algorithm/hackerrank/disjoint_set/conponentsinagraph/main.cpp
@@ -1,8 +1,199 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+class Node
 {
-    QCoreApplication a(argc, argv);
+public:
+    Node()
+        : data(-1)
+        , marked(false)
+    {}
 
-    return a.exec();
+    Node(int data)
+        : data(data)
+        , marked(false)
+    {}
+
+    bool contains(Node *node)
+    {
+        return std::find(lstAdjacent.begin(), lstAdjacent.end(), node) != lstAdjacent.end();
+    }
+
+    int data;
+    bool marked;
+    list<Node*> lstAdjacent;
+};
+
+class Graph
+{
+public:
+    Graph(){}
+
+    Node* getNode(int data)
+    {
+        for(int i = 0; i < vecNode.size(); i++)
+        {
+            if(data == vecNode[i]->data)
+                return vecNode[i];
+        }
+
+        return NULL;
+    }
+
+    bool contains(Node *node)
+    {
+        return std::find(vecNode.begin(), vecNode.end(), node) != vecNode.end();
+    }
+
+    void addEdge(Node *node1, Node *node2)
+    {
+        if(!contains(node1))
+        {
+            vecNode.push_back(node1);
+        }
+        if(!contains(node2))
+        {
+            vecNode.push_back(node2);
+        }
+
+        if(!node1->contains(node2))
+        {
+            node1->lstAdjacent.push_back(node2);
+        }
+
+        if(!node2->contains(node1))
+        {
+            node2->lstAdjacent.push_back(node1);
+        }
+    }
+
+    void initMark()
+    {
+        int index = 0;
+        while(index < vecNode.size())
+        {
+            vecNode[index]->marked = false;
+            index++;
+        }
+    }
+
+    int getSmallest()
+    {
+        int min = numeric_limits<int>::max();
+        for(Node *node : vecNode)
+        {
+            initMark();
+            node->marked  = true;
+            int height = minHeight(node);
+            if(min > height)
+                min = height;
+        }
+
+        return min + 1;
+    }
+
+    int getLargest()
+    {
+        int max = numeric_limits<int>::min();
+        for(Node *node : vecNode)
+        {
+            initMark();
+            node->marked  = true;
+            int height = maxHeight(node);
+            if(max < height)
+                max = height;
+        }
+
+        return max;
+    }
+
+    vector<Node*> vecNode;
+
+private:
+
+    int maxHeight(Node *node)
+    {
+        if(node == NULL)
+            return 0;
+
+        int count = 0;
+        int max = 0;
+        for(list<Node*>::iterator iter = node->lstAdjacent.begin(); iter != node->lstAdjacent.end(); iter++)
+        {
+            Node *node = (*iter);
+            if(!node->marked)
+            {
+                node->marked = true;
+                int height = maxHeight(node);
+                if(max < height)
+                    max = height;
+
+                count++;
+            }
+        }
+
+        return 1 + max;
+    }
+
+    int minHeight(Node *node)
+    {
+        if(node == NULL)
+            return 0;
+
+        int count = 0;
+        int min = 0;
+        for(list<Node*>::iterator iter = node->lstAdjacent.begin(); iter != node->lstAdjacent.end(); iter++)
+        {
+            Node *node = (*iter);
+            if(!node->marked)
+            {
+                node->marked = true;
+                int height = maxHeight(node);
+                if(min > height)
+                    min = height;
+
+                count++;
+            }
+        }
+
+        return 1 + min;
+    }
+};
+
+int main()
+{
+    int size = 0;
+    cin >> size;
+
+    int index = 0;
+    Graph graph;
+    while(index < size)
+    {
+        int index1, index2;
+        cin >> index1 >> index2;
+
+        Node *node1 = graph.getNode(index1);
+        if(NULL == node1)
+        {
+            node1 = new Node(index1);
+        }
+
+        Node *node2 = graph.getNode(index2);
+        if(NULL == node2)
+        {
+            node2 = new Node(index2);
+        }
+
+        graph.addEdge(node1, node2);
+
+        index++;
+    }
+
+    cout << graph.getSmallest() << " " << graph.getLargest() << endl;
+
+    return 0;
 }
--- algorithm/hackerrank/trees/topview/main.cpp
+++ algorithm/hackerrank/trees/topview/main.cpp
@@ -1,30 +1,306 @@
+#include <list>
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+
+#define TEST 1
+
 using namespace std;
 
-int main() {
+template <typename Type>
+class GraphNode
+{
+public:
+    GraphNode()
+        : marked(false)
+    {}
+    GraphNode(Type data)
+        : data(data)
+        , marked(false)
+    {}
 
-    int testcasecount;
-    cin >> testcasecount;
-
-    int index = 0;
-    while(index < testcasecount)
+    bool contains(GraphNode *node)
     {
-        int number;
-        cin >> number;
-        int current = 1;
-        int count = 0;
-        while((current * 3)<= number)
+        return std::find(lstAdjacent.begin(), lstAdjacent.end(), node) != lstAdjacent.end();
+    }
+
+    GraphNode<Type>* GraphNode<Type>::operator=(GraphNode &node)
+    {
+        return *this;
+    }
+
+    Type data;
+    bool marked;
+    list<GraphNode*> lstAdjacent;
+};
+
+template <typename Type>
+class Graph
+{
+public:
+    Graph()
+        : node_size(0)
+    {}
+    Graph(int node_size = 0)
+        : node_size(node_size)
+    {
+        vecNode.reserve(node_size);
+        int index = 0;
+        while(index < node_size)
         {
-            current *= 3;
-            count++;
-            cout << current << endl;
+            vecNode.push_back(new GraphNode<Type>(index));
+            index++;
+        }
+    }
+
+    void addEdge(int index1, int index2)
+    {
+        if(index1 >= node_size || index2 >= node_size)
+            return;
+
+        GraphNode<Type> *node1 = vecNode[index1];
+        GraphNode<Type> *node2 = vecNode[index2];
+
+        if(false == node1->contains(node2))
+        {
+            node1->lstAdjacent.push_back(node2);
         }
 
-        count += (number - current);
-
-        cout << count << endl;
-        index++;
+        if(false == node2->contains(node1))
+        {
+            node2->lstAdjacent.push_back(node1);
+        }
     }
-    //code
+
+    void addEdge(GraphNode<Type> *node1, GraphNode<Type> *node2)
+    {
+        if(node1 == NULL || node2 == NULL)
+            return;
+
+        if(false == node1->contains(node2))
+        {
+            node1->lstAdjacent.push_back(node2);
+        }
+
+        if(false == node2->contains(node1))
+        {
+            node2->lstAdjacent.push_back(node1);
+        }
+    }
+
+    void initMark()
+    {
+        int index = 0;
+        while(index < node_size)
+        {
+            GraphNode<Type>* node = vecNode[index];
+            if(node)
+            {
+                node->marked = false;
+            }
+            index++;
+        }
+    }
+
+    vector<GraphNode<Type>*> dfs(int index = 0)
+    {
+        vector<GraphNode<Type>*> vecGraphNode;
+
+        if(index >= node_size)
+            return vecGraphNode;
+
+        initMark();
+
+        GraphNode<Type> *node = this->vecNode[index];
+
+        stack<GraphNode<Type>*> stc;
+        stc.push(node);
+
+        node->marked = true;
+
+        while(false == stc.empty())
+        {
+            GraphNode<Type>* nodeTop = stc.top();
+            vecGraphNode.push_back(nodeTop);
+
+            stc.pop();
+
+            for(GraphNode<Type> *nodeFor : nodeTop->lstAdjacent)
+            {
+                if(nodeFor->marked == false)
+                {
+                    nodeFor->marked = true;
+                    stc.push(nodeFor);
+                }
+            }
+        }
+
+        return vecGraphNode;
+    }
+
+    void dfsPrint(int index = 0)
+    {
+        if(index >= node_size)
+            return;
+
+        initMark();
+
+        GraphNode<Type> *node = this->vecNode[index];
+
+        stack<GraphNode<Type>*> stc;
+        stc.push(node);
+
+        node->marked = true;
+
+        while(false == stc.empty())
+        {
+            GraphNode<Type>* nodeTop = stc.top();
+            cout << nodeTop->data << " ";
+
+            stc.pop();
+
+            for(GraphNode<Type> *nodeFor : nodeTop->lstAdjacent)
+            {
+                if(nodeFor->marked == false)
+                {
+                    nodeFor->marked = true;
+                    stc.push(nodeFor);
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+    vector<GraphNode<Type>*> bfs(int index = 0)
+    {
+        vector<GraphNode<Type>*> vecGraphNode;
+
+        if(index >= node_size)
+            return vecGraphNode;
+
+        initMark();
+
+        GraphNode<Type>* node = vecNode[index];
+        queue<GraphNode<Type>*> que;
+        que.push(node);
+
+        node->marked = true;
+
+        while(!que.empty())
+        {
+            GraphNode<Type>* nodeFront = que.front();
+            vecGraphNode.push_back(nodeFront);
+
+            que.pop();
+
+            for(GraphNode<Type>* nodeAdj : nodeFront->lstAdjacent)
+            {
+                if(!nodeAdj->marked)
+                {
+                    nodeAdj->marked = true;
+                    que.push(nodeAdj);
+                }
+            }
+        }
+
+        return vecGraphNode;
+    }
+
+#ifdef TEST
+    void dfsPrint(int index = 0)
+    {
+        if(index >= node_size)
+            return;
+
+        initMark();
+
+        GraphNode<Type> *node = this->vecNode[index];
+
+        stack<GraphNode<Type>*> stc;
+        stc.push(node);
+
+        node->marked = true;
+
+        while(false == stc.empty())
+        {
+            GraphNode<Type>* nodeTop = stc.top();
+            cout << nodeTop->data << " ";
+
+            stc.pop();
+
+            for(GraphNode<Type> *nodeFor : nodeTop->lstAdjacent)
+            {
+                if(nodeFor->marked == false)
+                {
+                    nodeFor->marked = true;
+                    stc.push(nodeFor);
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+    void bfsPrint(int index = 0)
+    {
+        if(index >= node_size)
+            return;
+
+        initMark();
+
+        GraphNode<Type>* node = vecNode[index];
+        queue<GraphNode<Type>*> que;
+        que.push(node);
+
+        node->marked = true;
+
+        while(!que.empty())
+        {
+            GraphNode<Type>* nodeFront = que.front();
+            cout << nodeFront->data << " ";
+
+            que.pop();
+
+            for(GraphNode<Type>* nodeAdj : nodeFront->lstAdjacent)
+            {
+                if(!nodeAdj->marked)
+                {
+                    nodeAdj->marked = true;
+                    que.push(nodeAdj);
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+#endif
+
+private:
+    int node_size;
+    vector<GraphNode<Type>*> vecNode;
+};
+
+int main()
+{
+    Graph<int> graph(9);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 5);
+    graph.addEdge(5, 6);
+    graph.addEdge(5, 7);
+    graph.addEdge(6, 8);
+
+    graph.bfsPrint();
+    graph.dfsPrint();
+
     return 0;
 }
