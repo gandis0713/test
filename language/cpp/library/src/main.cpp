4458bd0521cf9d1b5aea447039740a1c0d30f0ea
gandis <gandis0713@gmail.com>
2019-12-31 19:00:41 +0900

    modified that main

--- language/cpp/library/src/main.cpp
+++ language/cpp/library/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "../shared_1/global.h"
 #include "../shared_2/global.h"
-// #include "../static/global.h"
+#include "../static_1/global.h"
+#include "../static_2/global.h"
 using namespace std;
 
 
@@ -11,9 +12,10 @@ int main()
     cout << get_shared_1_global_num_reference() << endl;
     cout << get_shared_2_global_num() << endl;    
     cout << get_shared_2_global_num_reference() << endl;
-
-    // gnum++;
-
-    // cout << getsgnum() << endl;
+    cout << get_static_1_global_num() << endl;    
+    cout << get_static_1_global_num_reference() << endl;
+    cout << get_static_2_global_num() << endl;    
+    cout << get_static_2_global_num_reference() << endl;
+    
     return 0;
 }
