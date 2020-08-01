463d12b7d8f4d6e8500dcd82d92da6527e79083a
gandis <gandis0713@gmail.com>
2020-08-01 23:01:18 +0900

    test

--- tools/book_pilatese/book_requests.py
+++ tools/book_pilatese/book_requests.py
@@ -1,53 +1,66 @@
-# -*- encoding: utf-8 -*-
+import requests
 
-import requests, json
-from bs4 import BeautifulSoup as bs
+# User Info
+memberID = "7082"
+memberPW = "7082"
+
+# Site Info
+BaseURL = 'https://dagympilates2.flexgym.pro/mobile2'
+
+# Reservation Info
+# ( Fixed )
+payIdx = '2529299'
+# ( Variable )
+idx = "53924"
+SSIdx = '179556'
+Date = '2020-08-03'
+no = "6"
 
 # login.
-logURL = 'https://dagympilates2.flexgym.pro/mobile2/login.asp'
-logData = {'memberID': '7082', 'memberPW': '7082'}
+logURL = BaseURL + '/login.asp'
+logData = {'memberID': memberID, 'memberPW': memberPW}
 
-loginPayURL = 'https://dagympilates2.flexgym.pro/mobile2/login_pay.asp'
+loginedPageURL = BaseURL + '/login_pay.asp'
 
-reservationURL = 'https://dagympilates2.flexgym.pro/mobile2/reservation.asp'
-reservationData = {'codeSS033': '2529299'}
-reservationListData = {'codeSS033': '2529299', 'page': 1, 'Date': '2020-08-03', 'LC': 'C'}
+reservationPageURL = BaseURL + '/reservation.asp'
+reservationPageData = {'codeSS033': payIdx}
 
-reserveCheckURL = 'https://dagympilates2.flexgym.pro/mobile2/reservationChk.asp'
-reserveCheckData = {'params': "0|2020-07-09|36139|178531|24815|50274|2529299||||1|1"}
+ConfirmReservationURL = BaseURL + '/reservationChk.asp'
+ConfirmReservationData = {'params': "0|2020-07-09|36139|178531|24815|50274|2529299||||1|1"}
 
-reserveURL = 'https://dagympilates2.flexgym.pro/mobile2/reservation_appraise.asp'
-reserveData = {'params': "6|2020-08-03|53924|179556|24815|50274|2529299||||1|1"}
+selectReservationURL = BaseURL + '/reservation_appraise.asp'
+selectReservationData = {'params': no + '|' + Date + '|' + idx + '|' + SSIdx + '|24815|50274|2529299||||1|1'}
 
-reserveConfirmURL = 'https://dagympilates2.flexgym.pro/mobile2/reservation_appraise_ok.asp'
-reserveConfirmData = {'idx': "53924", 'payIdx': '2529299', 'SSIdx': '179556', 'Date': '2020-08-03'}
+reserveURL = BaseURL + '/reservation_appraise_ok.asp'
+reserveData = {'idx': idx, 'payIdx': payIdx, 'SSIdx': SSIdx, 'Date': Date}
 
+# # Login
 with requests.Session() as session:
-
-  with session.post(logURL, data=logData) as response:
+  with session.post(logURL, data = logData) as response:
     cookies = response.cookies
     headers = session.headers
-  # with session.post(loginPayURL, data=loginPayData, headers = headers, cookies=cookies) as response:
-    # print(session.cookies.get_dict())
-    # print(response.text)
-  # with session.post(reservationURL, data=reservationData, headers = headers, cookies=cookies) as response:
-  #   print(session.cookies.get_dict())
-  #   print(response.text)
-  # with session.post(reservationURL, data=reservationListData, headers = headers, cookies=cookies) as response:
-    # print(session.cookies.get_dict())
-    # print(response.text)
 
+# # Load Logined Page
 # with requests.Session() as session:
-#   with session.get(reserveCheckURL, data=reserveCheckData, headers= headers, cookies = cookies) as response:
+#   with session.get(loginedPageURL, headers = headers, cookies = cookies) as response:
 #     print(response.text)
 
+# Load reservation Page
 with requests.Session() as session:
-  with session.get(reserveURL, data=reserveData, headers= headers, cookies = cookies) as response:
+  with session.get(reservationPageURL, data = reservationPageData, headers = headers, cookies = cookies) as response:
     print(response.text)
 
-with requests.Session() as session:
-  with session.get(reserveConfirmURL, data=reserveConfirmData, headers= headers, cookies = cookies) as response:
-    print(response.status_code)
-    print(response.reason)
-    # print(response.text)
-    # print(response.text)
+# # Confirm reservation
+# with requests.Session() as session:
+#   with session.get(ConfirmReservationURL, data = ConfirmReservationData, headers = headers, cookies = cookies) as response:
+#     print(response.text)
+
+# # select reservation
+# with requests.Session() as session:
+#   with session.get(selectReservationURL, data = selectReservationData, headers =  headers, cookies = cookies) as response:
+#     print(response.text)
+
+# # reserve
+# with requests.Session() as session:
+#   with session.get(reserveURL, data = reserveData, headers =  headers, cookies = cookies) as response:
+#     print(response.text)
